4a498bcdc7802a2560b1b3f179638f15637ea249
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2025-09-14 10:09:04 -0500

    Add schema for Project Notes
    Added the schema and skeleton to add project notes.

--- Idea Vault/Item.swift
+++ Idea Vault/Item.swift
@@ -10,13 +10,16 @@ import SwiftData
 
 @Model
 final class Item {
+    var id: UUID = UUID()
     var lastUpdated: Date
     var createdDate: Date
     var title: String
+    var notes: [ItemNote]
     
-    init(lastUpdate: Date, created: Date, title: String) {
+    init(lastUpdate: Date, created: Date, title: String, notes: [ItemNote] = []) {
         self.lastUpdated = lastUpdate
         self.createdDate = created
         self.title = title
+        self.notes = notes
     }
 }
